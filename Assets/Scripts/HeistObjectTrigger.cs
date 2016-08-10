d46e8a7009de1bb0be0eef6189523660d2302953
Joshua Goller <jsgoller1@gmail.com>
2016-08-10 00:48:04 -0700

    - adds heist object prefab with color-changing script - updates cubearena to remove heist object, instead has exit point with color changer

---
+++ Assets/Scripts/HeistObjectTrigger.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HeistObjectTrigger : MonoBehaviour {
+
+	void OnTriggerEnter()
+	{
+        Renderer rend = GetComponent<Renderer>();
+		rend.material.color = new Color(Color.red.r, Color.red.b, Color.red.g, 1f);
+	}
+}
--- Assets/Scripts/Loader.cs
+++ Assets/Scripts/Loader.cs
@@ -11,6 +11,7 @@ public class Loader : MonoBehaviour {
 		// Load player and level
 		GameObject arena = Instantiate (Resources.Load ("CubeArena", typeof(GameObject)), new Vector3(0f, 0f ,0f), Quaternion.identity) as GameObject;
 		GameObject player = Instantiate (Resources.Load ("Player", typeof(GameObject)), new Vector3(-40f, 25f ,-15f), Quaternion.identity) as GameObject;
+		GameObject heistObject = Instantiate (Resources.Load ("HeistObject", typeof(GameObject)), new Vector3(10f, 25f ,35f), Quaternion.identity) as GameObject;
 		
 	}
 }
