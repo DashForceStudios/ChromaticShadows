1ee83d3bc6ad807e37c822fda3a22a3621e8c838
Joshua Goller <jsgoller1@gmail.com>
2016-08-09 20:54:22 -0700

    initial commit of lfs-tracked files

---
+++ Assets/Scripts/Loader.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityStandardAssets.Characters.ThirdPerson; // Also requires prototyping imports
+
+public class Loader : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+	{
+		// Load player and level
+		GameObject arena = Instantiate (Resources.Load ("CubeArena", typeof(GameObject)), new Vector3(0f, 0f ,0f), Quaternion.identity) as GameObject;
+		GameObject player = Instantiate (Resources.Load ("Player", typeof(GameObject)), new Vector3(-40f, 25f ,-15f), Quaternion.identity) as GameObject;
+		
+	}
+}
---
+++ Assets/Scripts/SceneControl.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class SceneControl : MonoBehaviour
+{
+
+    Scene currentScene;
+
+    // Use this for initialization
+    void Start()
+    {
+        currentScene = SceneManager.GetActiveScene();
+    }
+
+    // Reloads the scene with any variales reset to their default values.
+    public void RestartScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+        Debug.Log("Reloaded scene.");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            RestartScene(currentScene.name);
+        }
+    }
+}
